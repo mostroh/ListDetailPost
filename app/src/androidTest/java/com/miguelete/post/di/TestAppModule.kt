c0ce73bfe7a5a4f853a19cf9347e5538a7bab041
miguelete <belakor.l2@gmail.com>
2022-05-28 01:16:36 +0200

    UI Instrumentation test added

---
+++ app/src/androidTest/java/com/miguelete/post/di/TestAppModule.kt
@@ -0,0 +1,58 @@
+package com.miguelete.post.di
+
+import android.app.Application
+import androidx.room.Room
+import com.miguelete.post.data.database.PostDatabase
+import com.miguelete.post.data.server.PostService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [AppModule::class]
+)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) = Room.inMemoryDatabaseBuilder(
+        app,
+        PostDatabase::class.java
+    ).build()
+
+    @Provides
+    @Singleton
+    fun providePostDao(db: PostDatabase) = db.postDao()
+
+    @Provides
+    @Singleton
+    @ApiUrl
+    fun provideApiUrl(): String = "http://localhost:8080"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = HttpLoggingInterceptor().run {
+        level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder().addInterceptor(this).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteService(@ApiUrl apiUrl: String, okHttpClient: OkHttpClient): PostService {
+        return Retrofit.Builder()
+            .baseUrl(apiUrl)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
+    }
+}
---
+++ app/src/androidTest/java/com/miguelete/post/ui/MainInstrumentationTest.kt
@@ -0,0 +1,71 @@
+package com.miguelete.post.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.miguelete.post.R
+import com.miguelete.post.data.server.MockWebServerRule
+import com.miguelete.post.data.server.fromJson
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@ExperimentalCoroutinesApi
+@HiltAndroidTest
+class MainInstrumentationTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val mockWebServerRule = MockWebServerRule()
+
+    @get:Rule(order = 3)
+    val activityRule = ActivityScenarioRule(NavHostActivity::class.java)
+
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+
+    @Before
+    fun setUp() {
+        mockWebServerRule.server.enqueue(
+            MockResponse().fromJson("posts.json")
+        )
+
+        hiltRule.inject()
+
+        val resource = OkHttp3IdlingResource.create("OkHttp", okHttpClient)
+        IdlingRegistry.getInstance().register(resource)
+    }
+
+    @Test
+    fun clicking_post_navigates_to_detail() {
+
+        try {
+            Thread.sleep(2000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        onView(withId(R.id.recycler))
+            .perform(
+                actionOnItemAtPosition<RecyclerView.ViewHolder>(4, click()
+                )
+            )
+
+        onView(withId(R.id.header_content))
+            .check(matches(hasDescendant(withText("eum et est occaecati"))))
+
+    }
+}
